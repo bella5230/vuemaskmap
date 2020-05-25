a25ec5d228e51a59baf9f133f3f6b086f26840a6
bella5230 <babala5230@gmail.com>
2020-05-26 00:57:17 +0800

    first commit

---
+++ src/components/Title.vue
@@ -0,0 +1,26 @@
+<template>
+    <h1 class="title">{{ title }}</h1>
+</template>>
+
+<script>
+export default {
+    name:'Titlecomponent',
+    props:{
+      title:{
+        type: String,
+        reqireds:true
+        // default:"w1sss233?w"
+      }  
+    }
+    // data(){
+    //     return{
+    //         title : "hihihihhihihi title"
+    //     }
+    // }
+}
+</script>
+<style scoped>
+h1.title {
+color :blue
+}
+</style>
---
+++ src/components/inputcomponents/index.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    name:'input-component',
+    props:{
+      text:{
+        type: String,
+        reqireds:true
+
+      }  
+    },
+    computed:{
+      inputText:{
+        get(){
+          return this.text
+        },
+        set(val){
+          // console.log(val)
+          this.$emit('update:text',val)
+        }
+
+      }
+    }
+}
+</script>
+<template src="./template.html"></template>
+<style src="./style.css" scoped></style>
